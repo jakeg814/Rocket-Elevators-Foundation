bcf164f38c63cee72c2393a50cd1c2d53dd90da8
jakeg814 <jakegomez814@hotmail.com>
2021-12-01 14:16:37 -0500

    coaches saved me. form done

--- app/controllers/intervention_controller.rb
+++ app/controllers/intervention_controller.rb
@@ -3,35 +3,39 @@ class InterventionController < ApplicationController
  def index
  end
 
+ #need create method
+ def create
+ end
+
  # buildings related to a selected customer
  def get_building
     @buildings = Building.where("customer_id = ?", params[:id])
     respond_to do |format|
-      form.json { render :json => @buildings }
+      format.json { render :json => @buildings }
     end
  end
 
   # batteries related to a selected building
   def get_battery
-    @batteries = Battery.where(building_id, params[:id])
+    @batteries = Battery.where("building_id= ?", params[:id])
     respond_to do |format|
-      form.json { render :json => @batteries }
+      format.json { render :json => @batteries }
     end
   end
 
     # columnns related to a selected battery
     def get_column
-      @column = Column.where(battery_id, params[:id])
+      @column = Column.where("battery_id= ?", params[:id])
       respond_to do |format|
-        form.json { render :json => @column }
+        format.json { render :json => @column }
       end
     end
 
   # elevators related to a selected column
   def get_elevator
-    @elevators = Elevator.where(column_id, params[:id])
+    @elevators = Elevator.where("column_id= ?", params[:id])
     respond_to do |format|
-      form.json { render :json => @elevators }
+      format.json { render :json => @elevators }
     end
   end
 
@@ -39,7 +43,7 @@ class InterventionController < ApplicationController
    def get_employee
     @employee = Employee.all
     respond_to do |format|
-      form.json { render :json => @employee }
+      format.json { render :json => @employee }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ get "residential" => "home#residential"
 get "commercial" => "home#commercial"
 get "quote" => "home#quote"
 get "/intervention" => "home#intervention"
+get "/get_building/:id", to: 'intervention#get_building'
+get "/get_battery/:id", to: 'intervention#get_battery'
+get "/get_elevator/:id", to: 'intervention#get_elevator'
+get "/get_column/:id", to: 'intervention#get_column'
 post "getData" => "quote#getData"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
