a16f924ab4f69183517a3ae27da6a83e5a0fefd9
jakeg814 <jakegomez814@hotmail.com>
2021-12-03 09:34:27 -0500

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.16.0"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "JacobGomez"
-set :repo_url, "https://github.com/jakeg814/Rocket-Elevators-Foundation.git"
+set :repo_url, "https://github.com/jakeg814/Rocket-Elevators-Foundation"
 set :branch, 'main'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
