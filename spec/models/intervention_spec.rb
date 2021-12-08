babe409b76519da8ed6cc6a6b5d9c24b66758940
jakegomez814 <jakegomez814@hotmail.com>
2021-12-08 14:45:43 -0500

    intervention model tdd finished

--- spec/models/intervention_spec.rb
+++ spec/models/intervention_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 RSpec.describe Intervention, :type => :model do
-    it "is initialized with nil values" do
-        expect(Intervention.new).to_not be_valid
+    subject {
+        described_class.new(user_id: 1, customer_id: 1, building_id: 1, battery_id: 1, column_id: 1, elevator_id: 1, employee_id: 1, intervention_start: '2017-09-23', intervention_end: '2021-09-25', result: 'successful', report: 'intervention', status: 'idle')
+    }
+    it "is valid with valid attributes" do
+        expect(subject).to be_valid
     end
 
-    it "is not valid without an employee"
-    it "is not valid without a customer"
-    it "is not valid without blah blah"
-    it "is not valid without blah blah"
+    it "is not valid without a user" do
+        subject.user_id = nil
+        expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a customer" do
+        subject.customer_id = nil
+        expect(subject).to_not be_valid
+    end
+    it "is not valid without a building" do
+        subject.building_id = nil
+        expect(subject).to_not be_valid
+    end
+    it "is not valid without a battery" do
+        subject.battery_id = nil
+        expect(subject).to_not be_valid
+    end
 end
